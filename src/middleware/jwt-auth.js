7fda6b61cfb70052086c36862306d5c0b3bbe957
Andy <amgauthier1992@gmail.com>
2024-01-02 15:32:14 -0500

    changes type of header required for jwt validation

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -1,21 +1,39 @@
-const jwt = require( 'jsonwebtoken' );
+const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
 const logger = require('./logger');
 
 function validateJWT(req, res, next) {
-  const { session_token } = req.headers; 
-  jwt.verify(session_token, JWT_SECRET, function(err, decoded) {
-    if(err){
-      logger.error(err)
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+    return res.status(401).send('Unauthorized Request');
+  }
+
+  const token = authorizationHeader.split(' ')[1];
+
+  jwt.verify(token, JWT_SECRET, function (err, decoded) {
+    if (err) {
+      logger.error(err);
       return res.status(401).send('Unauthorized Request');
-    }
-    else {
-      req.user = decoded; //here we are adding the decoded info to the user 
-      //property of the request object. The auth header will be sent from the
-      //client side during the fetch calls
+    } else {
+      req.user = decoded;
       next();
     }
   });
+
+  // const { session_token } = req.headers;
+  // jwt.verify(session_token, JWT_SECRET, function(err, decoded) {
+  //   if(err){
+  //     logger.error(err)
+  //     return res.status(401).send('Unauthorized Request');
+  //   }
+  //   else {
+  //     req.user = decoded; //here we are adding the decoded info to the user
+  //     //property of the request object. The auth header will be sent from the
+  //     //client side during the fetch calls
+  //     next();
+  //   }
+  // });
 }
-  
-module.exports = validateJWT
+
+module.exports = validateJWT;
