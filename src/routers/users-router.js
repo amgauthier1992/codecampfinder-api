4ae3da6af7d7bb878c36149ec5ea5ff8befd4f1a
Andy <amgauthier1992@gmail.com>
2024-01-02 16:01:45 -0500

    reverts change made to jwt expiry, adjusts token references in other router methods

--- src/routers/users-router.js
+++ src/routers/users-router.js
@@ -14,9 +14,9 @@ const bodyParser = express.json();
 usersRouter
   .route('/api/users/register')
   .post(bodyParser, (req, res, next) => {
-    const { first_name, last_name, user_name, password } = req.body
-    const newUser = { first_name, last_name, user_name, password }
-    const knexInstance = req.app.get('db')
+    const { first_name, last_name, user_name, password } = req.body;
+    const newUser = { first_name, last_name, user_name, password };
+    const knexInstance = req.app.get('db');
     const saltRounds = 10;
 
     for (const [key, value] of Object.entries(newUser))
@@ -28,22 +28,22 @@ usersRouter
     UsersRepo.getUserWithUsername(knexInstance, user_name)
       .then(dbUser => {
         if (dbUser) {
-          return res.status(400).json({ error: 'Username already exists.' }) 
+          return res.status(400).json({ error: 'Username already exists.' }); 
         }
         else {
           //encrypt pw and overwrite plaintext pw on newUser object
           bcrypt.hash(password, saltRounds, function(err, hash) {
             if (err) {
-              logger.error(err)
+              logger.error(err);
               return res.status(400).send('Something went wrong.');
             }
             else {
               newUser.password = hash;
               UsersRepo.createUser(knexInstance, newUser)
               .then(newUser => {
-                return res.status(201).json(newUser)
+                return res.status(201).json(newUser);
               })
-              .catch(next)
+              .catch(next);
             }
           })
         }
@@ -53,9 +53,9 @@ usersRouter
 usersRouter
   .route('/api/users/login')
   .post(bodyParser, (req,res,next) => {
-    const { user_name, password } = req.body
-    const loginUser = { user_name, password }
-    const knexInstance = req.app.get('db')
+    const { user_name, password } = req.body;
+    const loginUser = { user_name, password };
+    const knexInstance = req.app.get('db');
 
     for (const [key, value] of Object.entries(loginUser))
       if (value == null)
@@ -66,13 +66,13 @@ usersRouter
     UsersRepo.getUserWithUsername(knexInstance, loginUser.user_name)
       .then(dbUser => {
         if (!dbUser) {
-          return res.status(400).json({ error: 'Incorrect user_name or password' }) 
+          return res.status(400).json({ error: 'Incorrect user_name or password' });
         }
         else { 
           UsersRepo.comparePasswords(loginUser.password, dbUser.password)
             .then(match => {
               if (!match)
-                return res.status(400).json({ error: 'Incorrect user_name or password' })
+                return res.status(400).json({ error: 'Incorrect user_name or password' });
               else {
                 UsersRepo.postLogin(knexInstance, user_name, dbUser.password)
                   const payload = {
@@ -80,7 +80,7 @@ usersRouter
                     first_name: dbUser.first_name,
                     last_name: dbUser.last_name
                   }
-                  jwt.sign(payload, JWT_SECRET, { expiresIn: '1m' }, function(err, token) {
+                  jwt.sign(payload, JWT_SECRET, { expiresIn: '30m' }, function(err, token) {
                     if (err) {
                       return res.status( 406 ).send( 'Something went wrong' );
                     }
@@ -90,7 +90,7 @@ usersRouter
                         username: loginUser.user_name, 
                         firstName: dbUser.first_name, 
                         lastName: dbUser.last_name 
-                      }) 
+                      }); 
                     }
                   })
               }
@@ -107,14 +107,14 @@ usersRouter
 usersRouter
   .route('/api/user/courses')
   .get(validateJWT, (req,res,next) => {
-    const token = jwt.decode(req.headers.session_token)
-    const knexInstance = req.app.get('db')
+    const token = jwt.decode(req.headers.authorization.split(' ')[1]);
+    const knexInstance = req.app.get('db');
 
     UsersRepo.getUserCourses(knexInstance, token.user_name)
       .then(userCourses => {
-        res.json(userCourses)
+        res.json(userCourses);
       })
-      .catch(next)
+      .catch(next);
   })
 
   .post(bodyParser, validateJWT, (req, res, next) => {
@@ -125,11 +125,11 @@ usersRouter
           error: `Missing '${key}' in request body`
         })
       
-      const bootcampName = req.body.Bootcamp
-      const courseName = req.body.Course.Name
-      const user_name = req.body.UserName
-      const schedule = req.body.Course.Schedule.Type
-      const knexInstance = req.app.get('db')
+      const bootcampName = req.body.Bootcamp;
+      const courseName = req.body.Course.Name;
+      const user_name = req.body.UserName;
+      const schedule = req.body.Course.Schedule.Type;
+      const knexInstance = req.app.get('db');
       
       //look up course by bootcamp and course name
       BootcampService.getBootcampAndCourse(knexInstance, bootcampName, courseName)
@@ -141,15 +141,15 @@ usersRouter
 
           CourseRepo.getScheduleByCourseAndSchedule(knexInstance, bootcamp.Course_id, schedule)
             .then(schedule => {
-              userCourse.schedule_type = schedule.id
+              userCourse.schedule_type = schedule.id;
 
               UsersRepo.addUserCourse(knexInstance, userCourse)
                 .then(course => {
-                  return res.status(201).json(course)
+                  return res.status(201).json(course);
                 })
-                .catch(next)
+                .catch(next);
             })
-            .catch(next)
+            .catch(next);
     
         })
   })
@@ -157,38 +157,38 @@ usersRouter
 usersRouter
   .route('/api/user/course/:course_id')
   .get(validateJWT, (req, res) => {
-    const knexInstance = req.app.get('db')
-    const { course_id } = req.params
+    const knexInstance = req.app.get('db');
+    const { course_id } = req.params;
 
     CourseService.getCourseInformation(knexInstance, course_id)
       .then(info => {
-        return res.json(info)
+        return res.json(info);
       })
   })
 
   .delete(validateJWT, (req, res, next) => {
-    const { course_id } = req.params
-    const token = jwt.decode(req.headers.session_token)
-    const knexInstance = req.app.get('db')
+    const { course_id } = req.params;
+    const token = jwt.decode(req.headers.authorization.split(' ')[1]);
+    const knexInstance = req.app.get('db');
 
     UsersRepo.getUserCourseSchedule(knexInstance, course_id)
       .then(schedule => {
-        const schedule_type = schedule.id
+        const schedule_type = schedule.id;
 
         UsersRepo.deleteUserCourse(knexInstance, course_id, token.user_name, schedule_type)
           .then(numRowsAffected => {
             if(numRowsAffected !== 1) {
-              logger.error(`Course with id ${course_id} not found.`)
+              logger.error(`Course with id ${course_id} not found.`);
               return res.status(404).json({
                 error: { message: `Course Not Found.`}
-              })
+              });
             }
-            logger.info(`Course with id ${course_id} deleted.`)
-            res.status(204).end() 
+            logger.info(`Course with id ${course_id} deleted.`);
+            res.status(204).end();
           })
       })
 
-      .catch(next)
+      .catch(next);
   })
 
 module.exports = usersRouter;
